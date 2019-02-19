d96c76dbd4408367c9464dc31193ba61b3b3eff6
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-02-19 12:33:58 -0700

    Made some changes get over it

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 // dropdown menu scripts
-function myFunction() {
+function dropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 // close dropdown when you click out of it
@@ -18,8 +18,54 @@ window.onclick = function(event) {
 
 // main page button scripts
 window.onload = function startButtonFunc() {
-  startButton = document.getElementById("startButton");
-  startButton.addEventListener("click", function jumpScroll() {
-    window.scroll(0, 1000);
-  });
+  if (document.getElementById("startButton")) {
+    let startButton = document.getElementById("startButton");
+    startButton.addEventListener("click", function jumpScroll() {
+      window.scroll(0, 1100);
+    });
+  } else {
+    console.log("Not purchase page, start button functionality revoked");
+  }
 };
+
+let findAgent = document.getElementById("findAgent");
+findAgent.addEventListener("click", function contacts() {
+  let agentName = [
+    "Jordan",
+    "Jake",
+    "Jessy",
+    "James",
+    "Adrian",
+    "Anton",
+    "Elena",
+    "Malkeet",
+    "Dani",
+    "Doug",
+    "Kuol",
+    "Kiefer",
+    "Muhommad"
+  ];
+
+  let agentNumbers = [
+    "(555)-555-9846",
+    "(555)-555-2365",
+    "(555)-555-7357",
+    "(555)-555-6779",
+    "(555)-555-1246",
+    "(555)-555-8325",
+    "(555)-555-9042",
+    "(555)-555-0568",
+    "(555)-555-3175",
+    "(555)-555-7653",
+    "(555)-555-3687",
+    "(555)-555-9960",
+    "(555)-555-9837"
+  ];
+  if (document.getElementById("results")) {
+    let i = Math.floor(Math.random() * 13 + 1);
+    let results = document.getElementById("results");
+    results.innerHTML = agentName[i] + " " + agentNumbers[i];
+  } else {
+    console.log("Not about page");
+  }
+});
